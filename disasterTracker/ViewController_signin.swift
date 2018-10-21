4aae92b835dfb5d38eb87d515506e0f16c05f064
GaelleMG <gaellemg1@gmail.com>
2018-10-21 14:14:55 -0400

    changed zoom params

--- disasterTracker/ViewController_signin.swift
+++ disasterTracker/ViewController_signin.swift
@@ -26,7 +26,7 @@ class ViewController_signin: UIViewController {
     
         print("----------- HERE 0 -------------")
         
-            let json: [String: Any] = [
+        let json: [String: Any] = [
                 "email" : inputEmail.text!,
                 "password" : inputPassword.text!
             ]
